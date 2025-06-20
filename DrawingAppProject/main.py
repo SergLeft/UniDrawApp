b8a0d330795ea8e0b924500d40084f5cb6423e2b
SergLeft <sleftheriotis@yahoo.com>
2025-06-20 22:21:38 +0200

    Delete DrawingAppProject directory

--- DrawingAppProject/main.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication
-from app.window import MyWindow
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    main_window = MyWindow()
-    main_window.show()
-    app.exec()
--- DrawingAppProject/utils/constants.py
+++
@@ -1,6 +0,0 @@
-from PySide6.QtGui import QColor
-
-# Default values
-DEFAULT_PEN_SIZE = 3
-DEFAULT_PEN_COLOR = Qt.black
-DEFAULT_CANVAS_SIZE = (800, 600)
